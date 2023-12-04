7b8e28b1116873c08fb2babd5ece5bef1fd2cb02
KimMinSung55 <generosity5542@gmail.com>
2023-12-04 23:49:06 +0900

    start

--- app/src/main/java/com/example/carrotmarket/ArticleAdapter.kt
+++ app/src/main/java/com/example/carrotmarket/ArticleAdapter.kt
@@ -19,54 +19,43 @@ class ArticleAdapter(status: String) : RecyclerView.Adapter<ArticleAdapter.ViewH
     private val db: FirebaseFirestore = Firebase.firestore
     private val missData = db.collection("article")
     private var itemlist: ArrayList<ItemData> = arrayListOf()
+
     init {
-        itemlist.clear()
         when (status) {
-//            "all" -> {
-//                missData.orderBy("uploadtime",Query.Direction.DESCENDING)
-//                    .addSnapshotListener { querySnapshot, _ ->
-//                        itemlist.clear()
-//                        for(snapshot in querySnapshot!!.documents){
-//                            var item = snapshot.toObject(ItemData::class.java)
-//                            itemlist.add(item!!)
-//                        }
-//                        notifyDataSetChanged()
-//                    }
-//            }
             "selling" -> {
                 missData.orderBy("uploadtime", Query.Direction.DESCENDING)
                     .addSnapshotListener { querySnapshot, _ ->
                         itemlist.clear()
-                        for (snapshot in querySnapshot!!.documents) {
-                            var item = snapshot.toObject(ItemData::class.java)
-                            if(item != null){
-                                val data = item?.sellvalue
-                                if(data != null){
-                                    if(data.contains("0", ignoreCase = true)){
-                                        itemlist.add(item!!)
+                        if (querySnapshot != null) {
+                            for (snapshot in querySnapshot.documents) {
+                                var item = snapshot.toObject(ItemData::class.java)
+                                if (item != null) {
+                                    val data = item.sellvalue
+                                    if (data != null && data.contains("0", ignoreCase = true)) {
+                                        itemlist.add(item)
                                     }
                                 }
                             }
+                            notifyDataSetChanged()
                         }
-                        notifyDataSetChanged()
                     }
             }
             "complete" -> {
                 missData.orderBy("uploadtime", Query.Direction.DESCENDING)
                     .addSnapshotListener { querySnapshot, _ ->
                         itemlist.clear()
-                        for (snapshot in querySnapshot!!.documents) {
-                            var item = snapshot.toObject(ItemData::class.java)
-                            if(item != null){
-                                val data = item?.sellvalue
-                                if(data != null){
-                                    if(data.contains("1", ignoreCase = true)){
-                                        itemlist.add(item!!)
+                        if (querySnapshot != null) {
+                            for (snapshot in querySnapshot.documents) {
+                                var item = snapshot.toObject(ItemData::class.java)
+                                if (item != null) {
+                                    val data = item.sellvalue
+                                    if (data != null && data.contains("1", ignoreCase = true)) {
+                                        itemlist.add(item)
                                     }
                                 }
                             }
+                            notifyDataSetChanged()
                         }
-                        notifyDataSetChanged()
                     }
             }
         }
@@ -88,35 +77,37 @@ class ArticleAdapter(status: String) : RecyclerView.Adapter<ArticleAdapter.ViewH
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        var viewholder = (holder as ViewHolder).itemView
-        holder.title.text = itemlist[position].title
-        holder.price.text = itemlist[position].price+"원"
-        if(itemlist[position].sellvalue == "0"){ //0이면 판매중
-            holder.value.text = "판매중"
-        } else {
-            holder.value.text = "판매완료"
-        }
+        val currentItem = itemlist.getOrNull(position) // null이 아닌 경우에만 사용
+        if (currentItem != null) {
+            val viewholder = (holder as ViewHolder).itemView
+            holder.title.text = currentItem.title
+            holder.price.text = "${currentItem.price}원"
 
+            if (currentItem.sellvalue == "0") {
+                holder.value.text = "판매중"
+            } else {
+                holder.value.text = "판매완료"
+            }
 
+            Glide.with(holder.itemimg)
+                .load(currentItem.uri)
+                .into(holder.itemimg)
 
-        Glide.with(holder.itemimg)
-            .load(itemlist[position].uri)
-            .into(holder.itemimg)
-
-        viewholder.setOnClickListener {
-            val context = viewholder.context
-            val intent = Intent(context,DetailActivity::class.java)
-            val inf = hashMapOf(
-                "title" to itemlist[position].title,
-                "price" to itemlist[position].price,
-                "uri" to itemlist[position].uri,
-                "id" to itemlist[position].sellerId,
-                "value" to itemlist[position].sellvalue,
-                "content" to itemlist[position].content,
-                "uid" to itemlist[position].uid,
-            )
-            intent.putExtra("info",inf)
-            context.startActivity(intent)
+            viewholder.setOnClickListener {
+                val context = viewholder.context
+                val intent = Intent(context, DetailActivity::class.java)
+                val inf = hashMapOf(
+                    "title" to currentItem.title,
+                    "price" to currentItem.price,
+                    "uri" to currentItem.uri,
+                    "id" to currentItem.sellerId,
+                    "value" to currentItem.sellvalue,
+                    "content" to currentItem.content,
+                    "uid" to currentItem.uid,
+                )
+                intent.putExtra("info", inf)
+                context.startActivity(intent)
+            }
         }
     }
 
